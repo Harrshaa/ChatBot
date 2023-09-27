26a42f439552c1298d371d8136adebb7793f8459
Harrshaa <sriharsha.mutakodoor@gmail.com>
2023-09-27 21:02:18 +0530

    Actons

--- src/chatbot/ActionProvider.js
+++ src/chatbot/ActionProvider.js
@@ -1,26 +1,73 @@
+// in ActionProvider.jsx
 import React from 'react';
+import { createClientMessage } from 'react-chatbot-kit';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  const initialAction = ()=>{
+    const message = createClientMessage("Got It!");
+    updateState(message);
+  }
 
-    const initialAction =()=>{
-        const message=createChatBotMessage('Got It');
-        updateState(message);
-    }
+  const updateState = (message, checker="") => {
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, message],
+      checker
+    }),
+    );
+  };
 
-    const updateState=(message) =>{
-        setState((prev) =>({
-            ...prev,
-            messages:[...prev.messages,message]
-        }))
-    }
+  const afterGotIt = ()=>{
+    const message = createChatBotMessage("Pick a slot", {
+      widget:"slots"
+    });
+    updateState(message);
+  }
+
+  // const displayTimings = ()=>{
+  //   const message = createChatBotMessage({
+  //     widget:"timings"
+  //   });
+  //   updateState(message);
+  // }
+
+  const timingMessage = (time)=>{
+    const message = createClientMessage(time.date+" "+time.time);
+    updateState(message);
+    enterName();
+  }
+
+
+  const enterName = ()=>{
+    const message = createChatBotMessage("Enter your name");
+    updateState(message, "age");
+  }
+
+  const enterAge = ()=>{
+    const message = createChatBotMessage("Enter your age",{
+      widget:"askage"
+    });
+    updateState(message, "exit");
+  }
+  const exitmsg = ()=>{
+    const message = createChatBotMessage("Thank you. In 5 seconds, bot will exit",{
+      widget:"exit"
+    });
+    updateState(message);
+  }
   
-  
-  
-    return (
+  return (
     <div>
       {React.Children.map(children, (child) => {
         return React.cloneElement(child, {
-          actions: {},
+          actions: {
+            initialAction,
+            afterGotIt,
+            timingMessage,
+            enterName,
+            enterAge,
+            exitmsg
+          },
         });
       })}
     </div>
